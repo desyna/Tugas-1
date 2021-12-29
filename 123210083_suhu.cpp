6d4da3984a007582ff79230eb641b0e1f1b07c73
desyna <123210083@student.upnyk.ac.id>
2021-12-29 19:32:47 +0700

    restore

--- 123210083_suhu.cpp
+++ 123210083_suhu.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 using namespace std;
@@ -13,9 +12,9 @@ int main()
     cout << "\nMasukkan Suhu Celcius : ";
     cin >> c;
 
-    f = (c * 9/5) + 32;
+    f = (c*9/5) + 32;
     k = 273 + c;
-    r = (c * 4/5);
+    r = (c*4/5) * c;
 
     cout << "\n" << c << " C = " << f << " F";
     cout << "\n" << c << " C = " << k << " K";
@@ -24,31 +23,4 @@ int main()
     cout << "\n\n\tThank You (^_^)";
 
     return 0;
-=======
-#include <iostream>
-
-using namespace std;
-int main()
-{
-    float c, f, k, r;
-    
-    int jumlah;
-
-    cout << "\tKonverter Suhu Celcius ke Fahrenheit, Kelvin, dan Reamur\n";
-    
-    cout << "\nMasukkan Suhu Celcius : ";
-    cin >> c;
-
-    f = (9/5 * c) + 32;
-    k = 273 + c;
-    r = (4/9 * c) + 32;
-
-    cout << "\n" << c << " C = " << f << " F";
-    cout << "\n" << c << " C = " << k << " K";
-    cout << "\n" << c << " C = " << r << " R";
-
-    cout << "\n\n\tThank You (^_^)";
-
-    return 0;
->>>>>>> de2367de21d168750dd31d6bd84e52bcb849f88c
 }
